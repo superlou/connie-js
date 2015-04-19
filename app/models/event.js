31157974c610803b6a47b99f24b707c73b760109
superlou <lousimons@gmail.com>
2015-04-18 21:07:01 -0400

    Start showing start and finish

--- app/models/event.js
+++ app/models/event.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
+  start: DS.attr('date'),
+  finish: DS.attr('date'),
   con: DS.belongsTo('con')
 });
