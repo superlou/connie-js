603d5156d8d01513bb493dfff4512f3021bce9b6
superlou <lousimons@gmail.com>
2015-04-19 15:43:38 -0400

    Clean up router

--- app/router.js
+++ app/router.js
@@ -9,15 +9,13 @@ Router.map(function() {
   this.route('con', {path: '/con/:con_id'}, function() {
     this.route('events', {path: '/events'}, function() {
       this.route('new', {path: '/new'});
-      this.route('event', {path: '/:event_id'});
+      this.route('show', {path: '/:event_id'});
       this.route('edit', {path: '/:event_id/edit'});
     });
 
     this.route('people');
-    this.route('person', {path: 'person/:event_id'});
 
     this.route('places');
-    this.route('place', {path: 'place/:event_id'});
   });
 
   this.route('map', {path: 'map/:map_id'});
